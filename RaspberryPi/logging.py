d1aace29d702b1d161c1f3147735d4030df1e280
TSimmz <tyler.simoni.8@gmail.com>
2020-02-04 20:33:46 -0500

    Adding logging file

---
+++ RaspberryPi/logging.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2.7
+
+import numpy as np
+from itertools import count
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+
+class Logging:
+    def __init__(self):
+        self.figure,self.axis = plt.subplots()
+        
+        self.timer = count()
+        self.timeElapsed = []
+        self.servo1Data = []
+        self.servo2Data = []
+        self.servo3Data = []
+        self.servo4Data = []
+        self.servo5Data = []
+        self.servo6Data = []
+        
+        self.axis
+        
+    def setData(alpha):
+        timeElapsed.append(next(timer))
+        servo1Data.append(alpha[0])
+        servo2Data.append(alpha[1])
+        servo3Data.append(alpha[2])
+        servo4Data.append(alpha[3])
+        servo5Data.append(alpha[4])
+        servo6Data.append(alpha[5])
+    
+    def animate(i):
+    
+    def graphData()
+        
+        
+        pltAni = FuncAnimation(plt.gcg(),
+        
+        plt.tight_layout()
+        plt.show()
+        
